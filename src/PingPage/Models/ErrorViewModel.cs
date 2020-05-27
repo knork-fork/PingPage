2611db853b8d1406f5b8f49b5af47bad6bc03392
Luka <looka1998@gmail.com>
2020-05-27 18:59:43 +0200

    Initial commit

---
+++ src/PingPage/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PingPage.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
