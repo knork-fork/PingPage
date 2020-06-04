acc74fa5c58f65fb83788b9b4a4e1c4c57c2f11c
Luka <looka1998@gmail.com>
2020-06-04 03:43:19 +0200

    Add user columns to registration form

---
+++ src/PingPage/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PingPage.DAL;
+using PingPage.Model;
+
+[assembly: HostingStartup(typeof(PingPage.Areas.Identity.IdentityHostingStartup))]
+namespace PingPage.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
